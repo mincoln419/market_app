43478e76402115b3a708dae3bfddb3a224674aca
mermer-mac <mincoln419@naver.com>
2024-03-23 06:50:49 +0900

    product detail ui complete

--- lib/home/product_detail_screen.dart
+++ lib/home/product_detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key});
@@ -16,18 +18,112 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
       ),
       body: Column(
         children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 320,
-                  color: Colors.orange,
-                  decoration: BoxDecoration(
-                    
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 320,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                    ),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(color: Colors.red),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 24,
+                              vertical: 12,
+                            ),
+                            child: Text(
+                              '할인중',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              '메르메르 인형',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                            PopupMenuButton(itemBuilder: (context) {
+                              return [
+                                const PopupMenuItem(
+                                  child: Text('리뷰등록'),
+                                ),
+                              ];
+                            }),
+                          ],
+                        ),
+                        const Text('제품상세정보'),
+                        const Text('상세 description'),
+                        const Row(
+                          children: [
+                            Text(
+                              '100000원',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.star,
+                              color: Colors.orange,
+                            ),
+                            Text('4.5'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  DefaultTabController(
+                      length: 2,
+                      child: Column(
+                        children: [
+                          const TabBar(
+                            tabs: [
+                              Tab(text: '제품상세',),
+                              Tab(text: '리뷰',),
+                            ],
+                          ),
+                          Container(
+                            height: 500,
+                            child: TabBarView(
+                              children: [
+                                Container(
+                                  child: Text('제품 상세'),
+                                ),
+                                Container(
+                                  child: Text('리뷰 화면'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),)
+                ],
+              ),
             ),
           )
         ],
