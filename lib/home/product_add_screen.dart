7b69c385f1f27122ba8ff52f5ec9d5801ce844af
mermer-mac <mincoln419@naver.com>
2024-03-23 10:59:49 +0900

    image picker 이미지 업로드

--- lib/home/product_add_screen.dart
+++ lib/home/product_add_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../model/category.dart';
 
 class ProductAddScreen extends StatefulWidget {
   const ProductAddScreen({super.key});
@@ -14,6 +19,13 @@ class _ProductAddScreenState extends State<ProductAddScreen> {
   final _formKey = GlobalKey<FormState>();
 
   bool isSale = false;
+
+  final db = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
+  Uint8List? imageData;
+  XFile? image;
+
+  Category? selectedCategory;
 
   TextEditingController titleTEC = TextEditingController();
   TextEditingController descriptionTEC = TextEditingController();
@@ -35,25 +47,36 @@ class _ProductAddScreenState extends State<ProductAddScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 240,
-                width: 240,
+            GestureDetector(
+              onTap: () async{
+                final ImagePicker picker = ImagePicker();
+                image = await picker.pickImage(source: ImageSource.gallery);
+                print('${image?.name}, ${image?.path}');
+                imageData = await image?.readAsBytes();
+                setState(() {
+
+                });
+              },
+              child: Align(
                 alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200]!,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                    color: Colors.grey,
+                child: Container(
+                  height: 240,
+                  width: 240,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200]!,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.add),
-                    Text('제품(상품) 이미지 추가'),
-                  ],
+                  child: imageData == null ? const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add),
+                      Text('제품(상품) 이미지 추가'),
+                    ],
+                  ): Image.memory(imageData!, fit: BoxFit.cover),
                 ),
               ),
             ),
